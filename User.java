6642f14f8ac96506a7932de8704c9612066605ed
Chloe Barnes <70238748+Chloe-Barnes74@users.noreply.github.com>
2024-03-29 17:05:49 -0400

    Update User.java
    Chloe edited

--- User.java
+++ User.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class User {
+public class User implements UserInterface {
     private String filename; // each user has a file to store user's data, "accountID.txt"
     private String accountID; // unique for every user, primary key
     private String password;
@@ -30,18 +30,22 @@ public class User {
         return filename;
     }
 
+    @Override
     public String getAccountID() {
         return accountID;
     }
 
+    @Override
     public String getUserName() {
         return userName;
     }
 
+    @Override
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
@@ -50,38 +54,47 @@ public class User {
         this.password = password;
     }
 
+    @Override
     public ArrayList<String> getFriendsList() {
         return friendsList;
     }
 
+    @Override
     public void setFriendsList(ArrayList<String> friendsList) {
         this.friendsList = friendsList;
     }
 
+    @Override
     public ArrayList<String> getBlockList() {
         return blockList;
     }
 
+    @Override
     public void setBlockList(ArrayList<String> blockList) {
         this.blockList = blockList;
     }
 
+    @Override
     public ArrayList<String> getMessageFilenames() {
         return messageFilenames;
     }
 
+    @Override
     public void setMessageFilenames(ArrayList<String> messageFilenames) {
         this.messageFilenames = messageFilenames;
     }
 
+    @Override
     public String getUserProfile() {
         return userProfile;
     }
 
+    @Override
     public void setUserProfile(String userProfile) {
         this.userProfile = userProfile;
     }
 
+    @Override
     public void downloadUser() {
         try (BufferedReader bfr = new BufferedReader(new FileReader(getFilename()))) {
             String line = "";
@@ -112,6 +125,7 @@ public class User {
         }
     }
 
+    @Override
     public void uploadUser() {
         try (PrintWriter pw = new PrintWriter(new FileWriter(getFilename()))) {
             pw.println(getUserName());
@@ -119,9 +133,11 @@ public class User {
             pw.println(getBlockList());
             pw.println(getMessageFilenames());
         } catch (IOException e) {
+            e.printStackTrace();
             return;
         }
     }
+
 
     public String toString() {
         return accountID + " " + password + " " + filename;
