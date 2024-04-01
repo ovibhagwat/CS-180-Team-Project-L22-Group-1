f4cfddf1a9e6670e7b70fe0a883b5507a74765a8
YiyangLiu1215 <165208791+YiyangLiu1215@users.noreply.github.com>
2024-03-31 21:32:17 -0400

    Create MessageDeletionException.java

---
+++ MessageDeletionException.java
@@ -0,0 +1,9 @@
+public class MessageDeletionException extends Exception {
+        public MessageDeletionException(String message) {
+            super(message);
+        }
+
+        public MessageDeletionException(String message, Throwable cause) {
+            super(message, cause);
+        }
+}
