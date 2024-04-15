a3e3eb785089686f41072d77e23232d07af11ef5
Chloe Barnes <70238748+Chloe-Barnes74@users.noreply.github.com>
2024-04-15 14:08:08 -0400

    Update ClientHandlerInterface.java

--- src/ClientHandlerInterface.java
+++ src/ClientHandlerInterface.java
@@ -4,4 +4,12 @@ import java.net.Socket;
  */
 public interface ClientHandlerInterface {
     void handleClient(Socket clientSocket);
+    RequestResponseProtocol.Response processLoginRequest(RequestResponseProtocol.Request request);
+    RequestResponseProtocol.Response processLogoutRequest(RequestResponseProtocol.Request request);
+    RequestResponseProtocol.Response processCreateAcctRequest(RequestResponseProtocol.Request request);
+    RequestResponseProtocol.Response processSendMsgRequest(RequestResponseProtocol.Request request);
+    RequestResponseProtocol.Response processBlockRequest(RequestResponseProtocol.Request request);
+    RequestResponseProtocol.Response processUnblockRequest(RequestResponseProtocol.Request request);
+    RequestResponseProtocol.Response processAddFriendRequest(RequestResponseProtocol.Request request);
+    RequestResponseProtocol.Response processRemoveFriendRequest(RequestResponseProtocol.Request request);
 }
